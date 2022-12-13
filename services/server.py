8d527ecdb3c60c23e19b2c9fcc1a87e52227034e
jancys <duchola1@gmail.com>
2022-12-13 17:08:10 +0100

    v.1.2

--- services/server.py
+++ services/server.py
@@ -2,9 +2,11 @@ import socket
 import json
 from threading import Thread
 from time import sleep
+import socketio
+
 #HOST i PORT to stałe które określają adres i port na którym nasłuchuje serwer
 HOST = "127.0.0.1"
-PORT = 6666
+PORT = 8484
 
 #Klasa Server obiekt tej klasy będzie nasłuchiwał na określonym porcie i adresie
 class Server(Thread):
@@ -12,12 +14,15 @@ class Server(Thread):
         super().__init__()
         self.s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.s.bind((HOST, PORT))
+        # self.s.bind((HOST, PORT))
+        # self.s.connect((HOST, PORT))
+        self.sio= socketio.Client()
+        self.sio.connect('http://localhost:8484')
         self.conn=None
         self.addr=None
         #Ilosc dozwolonych klientow
-        self.s.listen(1)
-        self.conn, self.addr = self.s.accept()
+        # self.s.listen(1)
+        # self.conn, self.addr = self.s.accept()
         #Lista samochodow
         self.car_list = []
 
@@ -42,30 +47,32 @@ class Server(Thread):
         return False
     #Zamykanie połączenia
     def close(self):
-        self.conn.close()
-        self.s.close()
+        # self.conn.close()
+        self.sio.disconnect()
     #Funkcja nasłuchująca na połączenie z klientem i odbierająca dane od klienta, uruchamiana w osobnym wątku
     def listen(self):
-        while True:
-            try:
-                data = self.conn.recv(1024)
-                if not data:
-                    break
-                print("Received: {}".format(data))
-            except:
-                break
+        self.sio.wait()
+        # while True:
+        #     try:
+            #     data = self.sio.recv()
+            #     if not data:
+            #         break
+            #     print("Received: {}".format(response))
+            # except:
+            #     break
         print("Connection closed")
         self.close()
     #Funkcja wysyłająca dane do klienta w formacie json
     def send_car_list(self,carListJson):
-            self.conn.send(json.dumps(carListJson).encode())
+            # self.sio.send(json.dumps(carListJson).encode())
+            self.sio.send(json.dumps(carListJson).encode())
 if __name__ == '__main__':
     server=Server()
     server.start()
     server.addCar("path/to/image","EWI 123","2019-01-01")
     sleep(5)
     server.removeCar("EWI 123")
-    server.close()
+    # server.close()
 
     server.join()
 
