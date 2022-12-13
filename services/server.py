7543b7a1d866b7212b7fe54040a1cda981d6508d
jancys <duchola1@gmail.com>
2022-12-13 17:14:26 +0100

    v.1.2

--- services/server.py
+++ services/server.py
@@ -14,16 +14,10 @@ class Server(Thread):
         super().__init__()
         self.s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        # self.s.bind((HOST, PORT))
-        # self.s.connect((HOST, PORT))
         self.sio= socketio.Client()
+
         self.sio.connect('http://localhost:8484')
-        self.conn=None
-        self.addr=None
-        #Ilosc dozwolonych klientow
-        # self.s.listen(1)
-        # self.conn, self.addr = self.s.accept()
-        #Lista samochodow
+        print("Connected to server")
         self.car_list = []
 
     def run(self):
@@ -47,24 +41,14 @@ class Server(Thread):
         return False
     #Zamykanie połączenia
     def close(self):
-        # self.conn.close()
         self.sio.disconnect()
     #Funkcja nasłuchująca na połączenie z klientem i odbierająca dane od klienta, uruchamiana w osobnym wątku
     def listen(self):
         self.sio.wait()
-        # while True:
-        #     try:
-            #     data = self.sio.recv()
-            #     if not data:
-            #         break
-            #     print("Received: {}".format(response))
-            # except:
-            #     break
         print("Connection closed")
         self.close()
     #Funkcja wysyłająca dane do klienta w formacie json
     def send_car_list(self,carListJson):
-            # self.sio.send(json.dumps(carListJson).encode())
             self.sio.send(json.dumps(carListJson).encode())
 if __name__ == '__main__':
     server=Server()
@@ -72,7 +56,7 @@ if __name__ == '__main__':
     server.addCar("path/to/image","EWI 123","2019-01-01")
     sleep(5)
     server.removeCar("EWI 123")
-    # server.close()
+    server.close()
 
     server.join()
 
