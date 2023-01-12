2656e29baf0bc50b8b9136d1c98a4e3b1a38079f
pabeler <pabeler@o2.pl>
2023-01-12 11:29:11 +0100

    Add functions to add or delete car from file contains all number plates

--- run_app.py
+++ run_app.py
@@ -14,17 +14,34 @@ from services.preprocessing.preprocessing import Preprocessing
 from services.ocr.ocr import Ocr
 from services.klient.klient import Server
 
+
+def add_car_name_to_file(name):
+    with open("car_base.txt", "w") as file:
+        file.write(name)
+        file.write('\n')
+
+
+def delete_car_name_from_file(name):
+    with open("car_base.txt", 'r') as file:
+        lines = file.readlines()
+
+    with open("car_base.txt", 'w') as file:
+        for line in lines:
+            if line.strip("\n") != name:
+                file.write(line)
+
+
 if __name__ == '__main__':
     client = Server()
-    h=0
+    h = 0
     preprocessing = Preprocessing('services/preprocessing/harascade_car.xml', 750)
     getImages("services/videoToImage/car1.mov")
     queue = services.videoToImage.main.Images
     print(queue.length())
     while not queue.isEmpty():
         tmp = queue.pop()
-        image=tmp[0]
-        path=tmp[1]
+        image = tmp[0]
+        path = tmp[1]
         plt.imshow(image)
         plt.show()
         try:
@@ -33,17 +50,15 @@ if __name__ == '__main__':
             ocr = Ocr(image)
             text = ocr.convert_preprocessed_image_to_text()
             if ocr.license_plate_validation():
+                add_car_name_to_file(text)
                 print(text)
-                path=f'/registers/photo{h}.jpg'
+                path = f'/registers/photo{h}.jpg'
                 cv2.imwrite(f'public/registers/photo{h}.jpg', tmp[0])
-                h+=1
-                client.addCar(path, text,datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                h += 1
+                client.addCar(path, text, datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
                 break
         except:
             continue
     # client.send_car_list(client.car_list)
     sleep(5)
     client.close()
-
-
-
